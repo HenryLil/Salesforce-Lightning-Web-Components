c4dc1afb0c3d2145df7d41b14ad5988606c9df16
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-07 18:50:08 +0300

    Exercise 2-4 Complete

---
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -0,0 +1,13 @@
+import { LightningElement, api} from 'lwc';
+
+export default class StudentTile extends LightningElement {
+    @api student = {
+        Name: 'Henry Lilleberg',
+        PhotoUrl: '/services/images/photo/003B0FakePictId',
+    };
+    @api selected = false;
+    
+    get tileSelected() {
+        return this.selected ? "tile selected" : "tile";
+    }
+}
