f598bf3b786d5c1269567b5ff7e7db770f3d77a5
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-08 23:44:32 +0300

    Extra credit 1 : complete

--- force-app/Exercises/main/default/lwc/challenge_currentDateTime/challenge_currentDateTime.js
+++ force-app/Exercises/main/default/lwc/challenge_currentDateTime/challenge_currentDateTime.js
@@ -2,9 +2,14 @@ import { LightningElement, track, api } from 'lwc';
 
 export default class Challenge_currentDateTime extends LightningElement {
     @track timestamp = new Date();
+    connectedCallback() {
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        setInterval(() => {this.refresh()}, 100);
+    }
 
     @api
     refresh() {
         this.timestamp = new Date();
     }
+    
 }
