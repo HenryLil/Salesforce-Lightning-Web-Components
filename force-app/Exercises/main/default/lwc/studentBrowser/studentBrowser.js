ec6e06a2cc8d342b03d0f8e3d66c3048435451f5
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-08 12:09:04 +0300

    Exercise 2-5 Few people

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class StudentBrowser extends LightningElement {}
+export default class StudentBrowser extends LightningElement {
+    @track studentList = [];
+
+    constructor() {
+        super();
+        const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', "Amit", "Simon"];
+        studentNames.forEach((studentName, index) => {
+          this.studentList.push(
+            {
+              'sobjectType' : 'Contact',
+              'Name' : studentName,
+              'PhotoUrl': '/services/images/photo/003B0FakePictId',
+              'Id' : index
+            }
+          );
+        })
+      }
+      
+}
