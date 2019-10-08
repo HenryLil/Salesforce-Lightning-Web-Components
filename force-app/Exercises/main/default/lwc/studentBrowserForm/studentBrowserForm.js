2b3c41e63249e1d350b986bcf4b18699c8ed7be3
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-08 18:29:19 +0300

    Exercise 2-9 Completed

--- force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
+++ force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
@@ -1,70 +1,82 @@
-import { LightningElement, api, track, wire } from 'lwc';
-import getInstructors from '@salesforce/apex/StudentBrowserForm.getInstructors';
-import getDeliveriesByInstructor from '@salesforce/apex/StudentBrowserForm.getDeliveriesByInstructor';
-export default class StudentBrowserForm extends LightningElement {
-    @track instructors = [];
-    error;
-    @track selectedInstructorId = '';
-    @track deliveries = [];
-    @track selectedDeliveryId = '';
+import { LightningElement, track, wire } from "lwc";
+import getInstructors from "@salesforce/apex/StudentBrowserForm.getInstructors";
+import getDeliveriesByInstructor from "@salesforce/apex/StudentBrowserForm.getDeliveriesByInstructor";
+import { NavigationMixin } from "lightning/navigation";
+export default class StudentBrowserForm extends NavigationMixin(LightningElement) {
+	@track instructors = [];
+	error;
+	@track selectedInstructorId = "";
+	@track deliveries = [];
+	@track selectedDeliveryId = "";
+	@track isButtonDisabled = true;
 
-    @wire(getInstructors)
-    wired_getInstructors({error, data}) {
-        this.instructors = [];
-        if(data) {
-            data.forEach(instructor => {
-                this.instructors.push({
-                    value: instructor.Id,
-                    label: instructor.Name
-                })
-         
-            });
-        }else if (error){
-            this.error = error;
-        }
-    }
-    
-    @wire(getDeliveriesByInstructor, {instructorId : '$selectedInstructorId'})
-    wired_getDeliveriesByInstructor({error, data}) {
-        this.deliveries = [];
-        if (data) {
-            if (this.selectedInstructorId) {
-                this.deliveries.push({
-                    value: '',
-                    label: 'Any delivery'
-                });
-            }
-            data.forEach(delivery => {
-                this.deliveries.push({
-                    value: delivery.Id,
-                    label: `${delivery.Start_Date__c} ${delivery.Location__c} ${delivery.Attendee_Count__c} students`
-                });
-            });
-        } else if (error) {
-            this.error = error;
-        }
-    }
+	@wire(getInstructors)
+	wired_getInstructors({ error, data }) {
+		this.instructors = [];
+		if (data) {
+			data.forEach(instructor => {
+				this.instructors.push({
+					value: instructor.Id,
+					label: instructor.Name
+				});
+			});
+		} else if (error) {
+			this.error = error;
+		}
+	}
 
-    onInstructorChange(event) {
-        this.selectedDeliveryId='';
-        this.selectedInstructorId=event.target.value;
-        this.notifyParent();
-    }
+	@wire(getDeliveriesByInstructor, { instructorId: "$selectedInstructorId" })
+	wired_getDeliveriesByInstructor({ error, data }) {
+		this.deliveries = [];
+		if (data) {
+			if (this.selectedInstructorId) {
+				this.deliveries.push({
+					value: "",
+					label: "Any delivery"
+				});
+			}
+			data.forEach(delivery => {
+				this.deliveries.push({
+					value: delivery.Id,
+					label: `${delivery.Start_Date__c} ${delivery.Location__c} ${delivery.Attendee_Count__c} students`
+				});
+			});
+		} else if (error) {
+			this.error = error;
+		}
+	}
 
-    onDeliveryChange(event){
-        this.selectedDeliveryId=event.target.value;
-        this.notifyParent();
-    }
+	onInstructorChange(event) {
+		this.selectedDeliveryId = "";
+		this.selectedInstructorId = event.target.value;
+		this.isButtonDisabled = this.selectedInstructorId === "";
+		this.notifyParent();
+	}
 
-    notifyParent() {
-        const evt = new CustomEvent('filterchange', {
-            detail: {
-                instructorId: this.selectedInstructorId,
-                deliveryId: this.selectedDeliveryId,
-            }
-        });
+	onDeliveryChange(event) {
+		this.selectedDeliveryId = event.target.value;
+		this.notifyParent();
+	}
 
-        this.dispatchEvent(evt);
-    }
+	notifyParent() {
+		const evt = new CustomEvent("filterchange", {
+			detail: {
+				instructorId: this.selectedInstructorId,
+				deliveryId: this.selectedDeliveryId
+			}
+		});
 
+		this.dispatchEvent(evt);
+	}
+
+	onAddNewDelivery() {
+		//opens the new course delivery record modal dialog
+		this[NavigationMixin.Navigate]({
+			type: "standard__objectPage",
+			attributes: {
+				objectApiName: "Course_Delivery__c",
+				actionName: "new"
+			}
+		});
+	}
 }
