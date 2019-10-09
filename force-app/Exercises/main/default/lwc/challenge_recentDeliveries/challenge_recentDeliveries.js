8961ef5fc7e2b0bca70f380569b88c288b71d69c
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-09 17:28:30 +0300

    Exercise 3-5 Lightning App

--- force-app/Exercises/main/default/lwc/challenge_recentDeliveries/challenge_recentDeliveries.js
+++ force-app/Exercises/main/default/lwc/challenge_recentDeliveries/challenge_recentDeliveries.js
@@ -2,14 +2,14 @@ import { LightningElement, wire } from 'lwc';
 //import { getRecentDeliveries } from "@salesforce/apex/CourseDeliveries.getRecentDeliveries";
 
 export default class Challenge_recentDeliveries extends LightningElement {
-    /*@wire(getRecentDeliveries, {})
+   /* @wire(getRecentDeliveries, {})
     recentDeliveries({error,data}) {
         this.recentDeliveries = [];
         if (data) {
             data.forEach(delivery => {
                 this.recentDeliveries.push({
                     value: delivery.Id,
-                    
+                    label: `${delivery.Location__c} on ${delivery.Start_Date__c}  ${delivery.Attendee_Count__c} students`
                 
                 })
             })
