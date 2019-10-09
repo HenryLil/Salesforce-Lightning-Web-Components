73e4461b793aa0b86fbf2e52b3c6f0f79cf05ed8
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-09 16:33:04 +0300

    Exercise 3-3 Complete

---
+++ force-app/Exercises/main/default/lwc/challenge_recentDeliveries/challenge_recentDeliveries.js
@@ -0,0 +1,19 @@
+import { LightningElement, wire } from 'lwc';
+//import { getRecentDeliveries } from "@salesforce/apex/CourseDeliveries.getRecentDeliveries";
+
+export default class Challenge_recentDeliveries extends LightningElement {
+    /*@wire(getRecentDeliveries, {})
+    recentDeliveries({error,data}) {
+        this.recentDeliveries = [];
+        if (data) {
+            data.forEach(delivery => {
+                this.recentDeliveries.push({
+                    value: delivery.Id,
+                    
+                
+                })
+            })
+            
+        }
+    } */
+}
---
+++ force-app/Exercises/main/default/lwc/layoutManager/layoutManager.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class LayoutManager extends LightningElement {}
