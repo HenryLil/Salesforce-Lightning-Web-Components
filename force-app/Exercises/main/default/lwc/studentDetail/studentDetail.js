e46c04bc58a14d6f6d80c9a8a7c7cf2f7e0d2e84
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-09 14:38:14 +0300

    Exercise 3-1 Created Wrapper Component and Action

--- force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
+++ force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
@@ -1,15 +1,15 @@
 import { LightningElement, track, wire } from "lwc";
 import { registerListener } from "c/pubsub";
-import { CurrentPageReference } from "lightning/navigation";
+import { CurrentPageReference, NavigationMixin } from "lightning/navigation";
 import { getRecord, getFieldValue, getFieldDisplayValue } from "lightning/uiRecordApi";
 import FIELD_Name from "@salesforce/schema/Contact.Name";
 import FIELD_Description from "@salesforce/schema/Contact.Description";
 import FIELD_Email from "@salesforce/schema/Contact.Email";
 import FIELD_Phone from "@salesforce/schema/Contact.Phone";
 
-const fields = [FIELD_Name, FIELD_Description, FIELD_Name, FIELD_Phone];
+const fields = [FIELD_Name, FIELD_Description, FIELD_Email, FIELD_Phone];
 
-export default class StudentDetail extends LightningElement {
+export default class StudentDetail extends NavigationMixin(LightningElement) {
 	@track studentId;
 	@wire(CurrentPageReference) pageRef;
 	@wire(getRecord, { recordId: "$studentId", fields }) wiredStudent;
@@ -36,5 +36,15 @@ export default class StudentDetail extends LightningElement {
 	}
 	_getDisplayValue(data, field) {
 		return getFieldDisplayValue(data, field) ? getFieldDisplayValue(data, field) : getFieldValue(data, field);
+	}
+
+	onGoToRecord() {
+		this[NavigationMixin.Navigate]({
+			type: "standard__recordPage",
+			attributes: {
+				recordId: "this.studentId",
+				actionName: "view"
+			}
+		});
 	}
 }
