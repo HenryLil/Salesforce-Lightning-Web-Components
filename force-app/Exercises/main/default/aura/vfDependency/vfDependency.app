a1213d7bfcfdaa52a830c7d44f9693e313894b63
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-09 18:48:04 +0300

    Exercise 3-6 Completed

---
+++ force-app/Exercises/main/default/aura/vfDependency/vfDependency.app
@@ -0,0 +1,5 @@
+<aura:application extends="ltng:outApp">
+    <aura:dependency resource="c:deliveryListMap"/>
+    <aura:dependency resource="lightning:badge"/>
+
+</aura:application>	
--- force-app/Exercises/main/default/lwc/challenge_recentDeliveries/challenge_recentDeliveries.js
+++ force-app/Exercises/main/default/lwc/challenge_recentDeliveries/challenge_recentDeliveries.js
@@ -6,12 +6,9 @@ export default class Challenge_recentDeliveries extends LightningElement {
     recentDeliveries({error,data}) {
         this.recentDeliveries = [];
         if (data) {
-            data.forEach(delivery => {
-                this.recentDeliveries.push({
-                    value: delivery.Id,
-                    label: `${delivery.Location__c} on ${delivery.Start_Date__c}  ${delivery.Attendee_Count__c} students`
-                
-                })
+            this.recentDeliveries = data;
+            this.error = undefined;
+                }else )
             })
             
         }
