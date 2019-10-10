0767d83fbacc05834dbde250d7e2c9637caa8309
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-10 13:53:45 +0300

    Exercise 4-4 Complete

--- force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
+++ force-app/Exercises/main/default/lwc/certifiedStudentList/certifiedStudentList.js
@@ -1,56 +1,97 @@
 import { LightningElement, api, track, wire } from "lwc";
 import getCertifiedStudents from "@salesforce/apex/CertifiedStudentList.getCertifiedStudents";
+import { refreshApex } from "@salesforce/apex";
+import deleteStudentCertification from "@salesforce/apex/CertifiedStudentList.deleteStudentCertification";
 
 export default class CertifiedStudentList extends LightningElement {
 	@api certificationId = 0;
 	@api certificationName = "";
-    @track certifiedStudents;
-    @track btnGroupDisabled = "disabled";
-    error;
-    
+	@track certifiedStudents;
+	@track btnGroupDisabled = "disabled";
+	error;
+	_wiredStudentResult;
+
 	@wire(getCertifiedStudents, { certificationId: "$certificationId" }) wired_getCertifiedStudents(result) {
-        this.certifiedStudents = [];
-        if (result.data) {
-        result.data.forEach(student => {
-        this.certifiedStudents.push({
-        certificationHeldId: student.Id,
-        contactId: student.Certified_Professional__r.Id,
-        name: student.Certified_Professional__r.Name,
-        date: student.Date_Achieved__c,
-        email: student.Certified_Professional__r.Email,
-        phone: student.Certified_Professional__r.Phone
-        });
-        });
-        } else if (result.error) {
-        this.error = result.error;
-        }
-    }
+		this._wiredStudentResult = result;
+		this.certifiedStudents = [];
+		if (result.data) {
+			result.data.forEach(student => {
+				this.certifiedStudents.push({
+					certificationHeldId: student.Id,
+					contactId: student.Certified_Professional__r.Id,
+					name: student.Certified_Professional__r.Name,
+					date: student.Date_Achieved__c,
+					email: student.Certified_Professional__r.Email,
+					phone: student.Certified_Professional__r.Phone
+				});
+			});
+		} else if (result.error) {
+			this.error = result.error;
+		}
+	}
 
-    columnConfig = [
-        {
-        label: 'Name',
-        fieldName: 'name',
-        type: 'text'
-        },
-        {
-        label: 'Date',
-        fieldName: 'date',
-        type: 'text'
-        },
-        {
-        label: 'Email',
-        fieldName: 'email',
-        type: 'email'
-        },
-        {
-        label: 'Phone',
-        fieldName: 'phone',
-        type: 'phone'
-        }
-    ];
+	columnConfig = [
+		{
+			label: "Name",
+			fieldName: "name",
+			type: "text"
+		},
+		{
+			label: "Date",
+			fieldName: "date",
+			type: "text"
+		},
+		{
+			label: "Email",
+			fieldName: "email",
+			type: "email"
+		},
+		{
+			label: "Phone",
+			fieldName: "phone",
+			type: "phone"
+		}
+	];
 
-    onRowSelection(event) {
-        let numSelected = event.detail.selectedRows.length;
-        this.btnGroupDisabled = (numSelected ===0);
-    }
+	onRowSelection(event) {
+		let numSelected = event.detail.selectedRows.length;
+		this.btnGroupDisabled = numSelected === 0;
+	}
+
+	getSelectedIDs() {
+		let datatable = this.template.querySelector("lightning-datatable");
+		let selectedRows = datatable.getSelectedRows();
+		let ids = [];
+
+		selectedRows.forEach(r => {
+			ids.push(r.certificationHeldId);
+		});
+		return ids;
+	}
+
+	onCertActions(event) {
+		const btnClicked = event.target.getAttribute("data-btn-id");
+		switch (btnClicked) {
+			case "btnEmail":
+				break;
+			case "btnSendCert":
+				break;
+			case "btnDelete":
+				this.onDelete();
+				break;
+			default:
+				break;
+		}
+	}
+
+	onDelete() {
+		let certificationIds = this.getSelectedIDs();
+		deleteStudentCertification({ certificationIds })
+			.then( () => {
+				refreshApex(this._wiredStudentResult);
+			})
+			.catch(error => {
+				this.error = error;
+			});
+	}
 }
