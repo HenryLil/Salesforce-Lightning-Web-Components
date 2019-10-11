c32202bcc1cf63badd3928b189a76e6439c1475f
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-11 13:45:28 +0300

    Exercise 6-2 Completed

--- force-app/Exercises/main/default/lwc/challenge_LineChart/challenge_LineChart.js
+++ force-app/Exercises/main/default/lwc/challenge_LineChart/challenge_LineChart.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
 
-export default class Challenge_LineChart extends LightningElement {}
+export default class Challenge_LineChart extends LightningElement {
+   /* @api apiURL ="";
+    error;
+    @track data;
+    fetch(ApiURL)
+        .then((result => {
+            if(!response.ok) {
+                this.error = response;
+            }
+            return response.json();
+        }
+        .then((jsonResponse) => {
+            this.someData = jsonResponse;
+        }
+        .catch(error => {
+            this.error = error;
+            this.data = undefined;
+        });
+        */
+
+}
