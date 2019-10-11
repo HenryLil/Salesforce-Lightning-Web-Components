fed944963ef34edd7b761096dddd71ed0e22ca8c
Henry Lilleberg <henry.liljestrom@gmail.com>
2019-10-11 12:11:43 +0300

    Exercise 5-6 Completed

--- force-app/Exercises/main/default/lwc/certPopularity/certPopularity.js
+++ force-app/Exercises/main/default/lwc/certPopularity/certPopularity.js
@@ -17,22 +17,44 @@ const CHART_CONFIG = {
 	}
 };
 export default class CertPopularity extends LightningElement {
-    error;
-    _chart;
-    _chartjsInitiallized=false;
-    renderedCallback() {
-        if (this._chartjsInitiallized) {
-            return;
-        }
-        this._chartjsInitiallized=true;
-        loadScript(this, chartjs)
-        .then(getCertPopularity)
-        .then ((result) => {
-            console.log('Data returned from Apex', result);
-            //todo:
-        })
-        .catch(error => {
-            this.error =error;
-        });
-    }
+	error;
+	_chart;
+	_chartjsInitiallized = false;
+	renderedCallback() {
+		if (this._chartjsInitiallized) {
+			return;
+		}
+		this._chartjsInitiallized = true;
+		loadScript(this, chartjs)
+			.then(getCertPopularity)
+			.then(result => {
+				console.log("Data returned from Apex", result);
+				let certData = result;
+				let certLabels = [];
+				let certCounts = [];
+				for (let i = 0; i < certData.length; i++) {
+					certLabels.push(certData[i].Name);
+					certCounts.push(certData[i].Number_of_Certified_Professionals__c);
+				}
+				const config = {
+					type: CHART_CONFIG.type,
+					data: {
+						labels: certLabels,
+						datasets: [
+							{
+								label: CHART_CONFIG.label,
+								backgroundColor: CHART_CONFIG.color,
+								data: certCounts
+							}
+						]
+					},
+					options: CHART_CONFIG.options
+				};
+				const ctx = this.template.querySelector(CHART_CONFIG.dom_selector).getContext("2d");
+				this._chart = new window.Chart(ctx, config);
+			})
+			.catch(error => {
+				this.error = error;
+			});
+	}
 }
